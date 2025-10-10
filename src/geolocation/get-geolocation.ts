4b238d74665a0afecdf7bee31a7d9895ed5b7c46
Örsan Günay <oersan.guenay@baywa.de>
2025-10-10 15:59:47 +0200

    Geolocation Solution

--- src/geolocation/get-geolocation.ts
+++ src/geolocation/get-geolocation.ts
@@ -4,18 +4,17 @@ async function fetchAddress(lat: number, lon: number) {
   return addressData;
 }
 
-function getCurrentPosition(options?: PositionOptions) {
+function getCurrentPosition(options?: PositionOptions): Promise<GeolocationPosition> {
   return new Promise<GeolocationPosition>((resolve, reject) => {
     if (!('geolocation' in navigator)) {
       reject(new Error('Geolocation is not supported by your browser.'));
-      return;
     }
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
 }
 
 async function getGeoLocation() {
-  const pos = await getCurrentPosition();
+  const pos: Awaited<GeolocationPosition> = await getCurrentPosition();
 
   const { latitude, longitude } = pos.coords;
   return fetchAddress(latitude, longitude);
